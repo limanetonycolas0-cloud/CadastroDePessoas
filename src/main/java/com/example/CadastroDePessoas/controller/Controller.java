59e742a409c67fc38092b35ba273220df13198fd
arkazz <limanetonycolas0@gmail.com>
2026-01-09 22:49:47 -0300

    Controller Created

---
+++ src/main/java/com/example/CadastroDePessoas/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.example.CadastroDePessoas.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "This is my first message";
+    }
+}
