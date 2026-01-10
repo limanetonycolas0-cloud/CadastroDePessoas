0027dc8057fdfbc7a79ce8a07426706aafdad8fd
arkazz <limanetonycolas0@gmail.com>
2026-01-09 23:28:35 -0300

    Attributes added

---
+++ src/main/java/com/example/CadastroDePessoas/model/PeopleModel.java
@@ -0,0 +1,61 @@
+package com.example.CadastroDePessoas.model;
+
+public class PeopleModel {
+    private String name;
+    private int age;
+    private String job;
+    private double salary;
+    private String city;
+
+
+    PeopleModel(){
+    }
+
+    PeopleModel(String name, int age, String job, double salary, String city){
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.salary = salary;
+        this.city = city;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
